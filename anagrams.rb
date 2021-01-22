fb4b18a5a1e8befdcc530a93130eb52c2964e568
Parth Shah <pcs003@ucsd.edu>
2021-01-22 15:27:30 -0500

    completed anagrams

--- anagrams.rb
+++ anagrams.rb
@@ -29,3 +29,38 @@ end
 # p second_anagram?("elvis", "lives")    #=> true
 
 #second_agagram time complexity is O(n*m) and space complexity is O(n) 
+
+def third_anagram?(string1, string2)
+    sorted1 = string1.chars.sort.join
+    sorted2 = string2.chars.sort.join
+
+    sorted1 == sorted2
+end
+
+# p third_anagram?("gizmo", "sally")    #=> false
+# p third_anagram?("elvis", "lives")    #=> true
+
+# third_anagram time complexity is O(nlogn) and space complexity is O(n)
+
+
+
+def fourth_anagram?(string1, string2)
+    hash1 = Hash.new(0)
+    # hash2 = Hash.new(0)
+
+    string1.each_char do |char|
+        hash1[char] += 1
+    end
+    string2.each_char do |char|
+        #hash2[char] += 1
+        hash1[char] -= 1
+    end
+
+    # hash1 == hash2
+    hash1.all? { |key, value| value == 0 }
+end
+
+p fourth_anagram?("gizmo", "sally")    #=> false
+p fourth_anagram?("elvis", "lives")    #=> true
+
+# fourth_anagram time complexity is O(n) and space complexity is O(n)
