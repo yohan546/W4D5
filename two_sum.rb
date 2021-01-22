9759d766b9ede624f9f0a3722ef6579615ff23f0
yohan oh <yohan.oh332@gmail.com>
2021-01-22 16:24:58 -0500

    finished two_sum

--- two_sum.rb
+++ two_sum.rb
@@ -7,6 +7,56 @@ def bad_two_sum?(arr, target)
     false
 end
 
+# arr = [0, 1, 5, 7]
+# p bad_two_sum?(arr, 6) # => should be true
+# p bad_two_sum?(arr, 10) # => should be false
+
+# bad_two_sum? time complexity => O(n^2) and space complexity => none
+
+def okay_two_sum?(arr, target)
+    sorted = arr.sort
+
+    left = 0
+    right = arr.length - 1 
+
+    while left < right 
+        sum = arr[left] + arr[right]
+
+        if sum == target
+            return true
+        elsif sum < target 
+            left += 1 
+        else
+            right -= 1
+        end
+    end
+
+    false
+end
+
+# arr = [0, 1, 5, 7]
+# p okay_two_sum?(arr, 6) # => should be true
+# p okay_two_sum?(arr, 10) # => should be false
+
+# okay_two_sum?, time complexity => O(n *logn), space complexity => O(n)
+
+def hash_two_sum?(arr, target)
+    hash = Hash.new
+
+    arr.each do |ele|
+        return true if hash.has_key?(target - ele) 
+        hash[ele] = 0
+    end
+
+    false
+end 
+
+# target = 6
+# array = [0,1,5,7]
+# hash = {}
+
+# {0:0, 1:0, return true}
+
 arr = [0, 1, 5, 7]
-p bad_two_sum?(arr, 6) # => should be true
-p bad_two_sum?(arr, 10) # => should be false
+p hash_two_sum?(arr, 6) # => should be true
+p hash_two_sum?(arr, 10) # => should be false
