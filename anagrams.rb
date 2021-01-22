1c8dd7a736e625ec64271dd1b3380665a69c8052
yohan oh <yohan.oh332@gmail.com>
2021-01-22 15:12:56 -0500

    finished first and second anagram

---
+++ anagrams.rb
@@ -0,0 +1,31 @@
+def first_anagram?(string1, string2) 
+
+    string1.chars.permutation.to_a.map(&:join).include?(string2)
+    # p char_arrays
+    # perms = []
+    # char_arrays.each do |char_array|
+    #     cur_perm = char_array
+    #     perms << cur_perm if !perms.include?(cur_perm)
+    # end
+
+    # perms.include?(string2) 
+end
+
+# first_anagram?("gizmo", "sally")    #=> false
+# first_anagram?("elvis", "lives")    #=> true
+
+#first_anagram time and space complexity is O(n! * n) 
+
+def second_anagram?(string1, string2)
+    string1.each_char do |char| #Q, does this create new memory?
+        if string2.include?(char) 
+            string2.slice!(char)
+        end
+    end
+    return string2.empty?
+end
+
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")    #=> true
+
+#second_agagram time complexity is O(n*m) and space complexity is O(n) 
